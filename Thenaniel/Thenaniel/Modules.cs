7215f2c476dab105269601e7bb9ed5af8271923b
Darvin Muralitharan <muralitharan.darvin@gmail.com>
2022-10-10 22:02:10 -0400

    8 ball command setup

--- Thenaniel/Thenaniel/Modules.cs
+++ Thenaniel/Thenaniel/Modules.cs
@@ -7,38 +7,31 @@ namespace Thenaniel
 {
 	public class SayModule : ModuleBase<SocketCommandContext>
 	{
-        //[Command("ask")]
-        //[Summary("Ask the Magic 8 Ball")]
-        //public async Task AskAsync()
-        //{
-        //	// randomly select response
-        //	Random magicBall = new Random();
+        [Command("ask")]
+        [Summary("Ask the Magic 8 Ball")]
+        public async Task AskAsync([Remainder] string question)
+        {
+        	// randomly select response
+        	Random magicBall = new Random();
 
-        //	int num = magicBall.Next(0, 10);
+        	int num = magicBall.Next(0, 10);
 
-        //	string[] response =
-        //	{
-        //		"```Definitely Not```",
-        //		"```Maybe```",
-        //		"```God No```",
-        //		"```Sure```",
-        //		"```Go For It Champ```",
-        //		"```Ask Again Later```",
-        //		"```I Doubt It```",
-        //		"```Why Not```",
-        //		"```Yes```",
-        //		"```Most Likely```",
-        //	};
+        	string[] response =
+        	{
+        		"```Definitely Not```",
+        		"```Maybe```",
+        		"```God No```",
+        		"```Sure```",
+        		"```Go For It Champ```",
+        		"```Ask Again Later```",
+        		"```I Doubt It```",
+        		"```Why Not```",
+        		"```Yes```",
+        		"```Most Likely```",
+        	};
 
-        //await ReplyAsync(response[num]);
-        //}
-        // ~say hello world -> hello world
-        [Command("say")]
-        [Summary("Echoes a message.")]
-        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
-            => ReplyAsync(echo);
-
-        // ReplyAsync is a method on ModuleBase
+        await ReplyAsync(response[num]);
+        }
     }
 
 	// Commands related to league
